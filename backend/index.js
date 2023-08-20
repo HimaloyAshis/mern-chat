1441578cc6746f9cf0b5cc0609922455fccc31cc
himaloyash <himaloyashis98@gmail.com>
2023-08-20 11:26:23 +0600

    chat is going on

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 5000
+
+
+
+app.get('/', (req, res)=>{
+    res.send('The chat group will going to be opened soon')
+})
+
+
+app.listen(port, ()=>{
+    console.log(`The chat group will be opened soon on port: ${port}`)
+})
+
