80f473054cf4c5ea1fec3c82555edb5e7862d148
himaloyash <himaloyashis98@gmail.com>
2023-08-20 12:40:04 +0600

    json file added

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-const chat = require('.data/data.js')
+const chat = require("../backend/data/data")
 
 
 
 
 app.get('/', (req, res)=>{
     res.send('The chat group will going to be opened soon')
+})
+
+app.get('/app/chat', (req, res)=>{
+    res.send(chat)
+})
+
+
+app.get('/app/chat/:id', (req, res)=>{
+    const singleChat = chat.find((c)=>c._id === req.params.id)
+    res.send(singleChat)
 })
 
 
