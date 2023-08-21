899d6ccebcac9e974874a4a25fa3590e925f1706
himaloyash <himaloyashis98@gmail.com>
2023-08-21 16:56:46 +0600

    sign up page done

--- frontend/mern-chats/src/page/Home/Home.jsx
+++ frontend/mern-chats/src/page/Home/Home.jsx
@@ -1,10 +1,46 @@
-import {Container} from '@chakra-ui/react'
+import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react'
 import React from 'react';
 
 const Home = () => {
     return (
         <Container maxW={'xl'} centerContent>
-            
+            <Box
+                d={'flex'}
+                justifyContent={'center'}
+                bg={'white'}
+                w={'100%'}
+                borderRadius={'lg'}
+                borderWidth={'1px'}
+                m={'40px 0 15px 0'}
+                p={'1rem'}
+            >
+
+                <Text fontSize={'4xl'}>Chat with your</Text>
+
+            </Box>
+
+            <Box
+                bg={'white'}
+                w={'100%'}
+                borderRadius={'lg'}
+                borderWidth={'1px'}
+                p={'1rem'}>
+                <Tabs variant='enclosed'>
+                    <TabList>
+                        <Tab>Sign Up</Tab>
+                        <Tab>Login</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel>
+                            <p>one!</p>
+                        </TabPanel>
+                        <TabPanel>
+                            <p>two!</p>
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
+
+            </Box>
 
         </Container>
     );
