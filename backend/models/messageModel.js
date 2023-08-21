25883491b6bf17cddff0c6397906a0d8f6fcf9ff
himaloyash <himaloyashis98@gmail.com>
2023-08-21 13:04:28 +0600

    message schema created

---
+++ backend/models/messageModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const messageModel = mongoose.Schema(
+    {
+        sender: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
+        content: {type: String, trim: true},
+        chat : {type: mongoose.Schema.Types.ObjectId, ref: 'chat'},
+    },
+    {
+        timeStamps: true
+    }
+)
